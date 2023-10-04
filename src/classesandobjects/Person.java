c5570979de8613bb5687768df79fe5816b8bcc1d
2022429 <146199262+2022429@users.noreply.github.com>
2023-10-04 13:16:23 +0100

    Added and input statements for the new shapes packages and triangle class

---
+++ src/classesandobjects/Person.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classesandobjects;
+
+import shapes.Triangle;
+
+/**
+ *
+ * @author sadiq
+ */
+public class Person {
+     
+   String name;
+   int age;
+   double height;
+   boolean isprogrammer = true;
+   Triangle favTriangle; //favourite triangle
+   
+    
+}
